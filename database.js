481499eb9e328d7afe4d7f6f15b5aaf133972e50
Kevin <kevin>
2017-06-09 16:07:48 +0300

    Add transaction methods

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
+const request = require('request');
 
 module.exports = class Database {
     connect(options, callback) {
@@ -25,5 +26,26 @@ module.exports = class Database {
 
         this._connected = true;
         callback();
+    }
+
+    trBegin(options, callback) {
+        this._post(`/${options.database}/transaction/begin`, {
+            type: 'form',
+            auth: options.auth
+        }, callback);
+    }
+
+    trCommit(options, callback) {
+        this._post(`/${options.database}/transaction/commit/` + options.tid, {
+            type: 'form',
+            auth: options.auth
+        }, callback);
+    }
+
+    trRollback(options, callback) {
+        this._post(`/${options.database}/transaction/rollback/` + options.tid, {
+            type: 'form',
+            auth: options.auth
+        }, callback);
     }
 };
